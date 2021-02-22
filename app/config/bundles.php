45f5e92230bae7128269feceef5f3323d937e03b
Alex Rundquist <alrund@gmail.com>
2021-02-22 14:25:47 +0300

    Install symfony/maker-bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
