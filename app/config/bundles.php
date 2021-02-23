acbac2a3823ed8b9634525416dd1ee34f3b051ed
Alex Rundquist <alrund@gmail.com>
2021-02-23 17:46:40 +0300

    Install doctrine/orm, doctrine-bundle, doctrine-migrations-bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -6,4 +6,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
