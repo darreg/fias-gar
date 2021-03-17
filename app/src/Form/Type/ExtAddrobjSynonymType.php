a0c4af2d3f2a15a95a398b7fc8c9faed144e64c0
Alex Rundquist <alrund@gmail.com>
2021-03-17 20:19:21 +0300

    Add form types

---
+++ app/src/Form/Type/ExtAddrobjSynonymType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\ExtAddrobjSynonym;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExtAddrobjSynonymType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => '',
+                'attr' => [
+                    'required' => true,
+                    'placeholder' => 'Наименование',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ExtAddrobjSynonym::class
+        ]);
+    }
+}
