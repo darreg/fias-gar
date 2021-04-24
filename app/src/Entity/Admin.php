79fcde2a8587a39d68381396f80ddbde105de63e
Alex Rundquist <alrund@gmail.com>
2021-04-24 18:15:31 +0300

    Add migration for security entities

--- app/src/Entity/Admin.php
+++ app/src/Entity/Admin.php
@@ -7,7 +7,17 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
+ * @ORM\Table(
+ *     name="admin",
+ *     uniqueConstraints={
+ *         @ORM\UniqueConstraint(
+ *              name="admin__unique__constraint",
+ *              columns={"email"}
+ *          )
+ *     }
+ * )
  * @ORM\Entity(repositoryClass=AdminRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  */
 class Admin implements UserInterface
 {
@@ -20,23 +30,23 @@ class Admin implements UserInterface
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private ?int $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      */
-    private $email;
+    private ?string $email;
 
     /**
      * @ORM\Column(type="json")
      */
-    private $roles = [];
+    private array $roles = [];
 
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
      */
-    private $password;
+    private string $password;
 
     public function getId(): ?int
     {
