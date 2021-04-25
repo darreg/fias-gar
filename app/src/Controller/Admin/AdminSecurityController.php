269426e7c5d3cb9601b463085c8221ea84fef121
Alex Rundquist <alrund@gmail.com>
2021-04-25 18:43:50 +0300

    Add login form

---
+++ app/src/Controller/Admin/AdminSecurityController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminSecurityController extends AbstractController
+{
+    /**
+     * @Route("/admin/login", name="admin_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        //security/admin-login.html.twig
+        return $this->render('admin/security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'csrf_token_intention' => 'authenticate',
+            'username_parameter' => 'email',
+            'password_parameter' => 'password',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/logout", name="admin_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException(
+            'This method can be blank - it will be intercepted by the logout key on your firewall.'
+        );
+    }
+}
