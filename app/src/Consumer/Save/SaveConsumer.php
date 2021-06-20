7d127868a0ce8a6929496d50333fc6508a19a5c3
Alex Rundquist <alrund@gmail.com>
2021-06-20 21:55:08 +0300

    Add rabbitmq consumers

---
+++ app/src/Consumer/Save/SaveConsumer.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Consumer\Save;
+
+use App\Consumer\Save\Input\Message;
+use App\Service\FiasImport\FiasImportXmlService;
+use Doctrine\ORM\EntityManagerInterface;
+use JsonException;
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use PhpAmqpLib\Message\AMQPMessage;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+final class SaveConsumer implements ConsumerInterface
+{
+    private EntityManagerInterface $entityManager;
+
+    private ValidatorInterface $validator;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator
+    ) {
+        $this->entityManager = $entityManager;
+        $this->validator = $validator;
+    }
+
+    public function execute(AMQPMessage $msg): int
+    {
+        try {
+            $message = Message::createFromQueue($msg->getBody());
+            $errors = $this->validator->validate($message);
+            if ($errors->count() > 0) {
+                return $this->reject((string)$errors);
+            }
+        } catch (JsonException $e) {
+            return $this->reject($e->getMessage());
+        }
+
+        FiasImportXmlService::parse($message->getXml());
+
+//        $userRepository = $this->entityManager->getRepository(User::class);
+
+
+        $this->entityManager->clear();
+        $this->entityManager->getConnection()->close();
+
+        return self::MSG_ACK;
+    }
+
+    private function reject(string $error): int
+    {
+        echo "Incorrect message: $error";
+
+        return self::MSG_REJECT;
+    }
+}
