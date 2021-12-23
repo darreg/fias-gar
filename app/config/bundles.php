e7753cdb4eb519c6e28ef1f623ed01501550d88b
Alex Rundquist <alrund@gmail.com>
2021-12-23 14:23:33 +0300

    Remove rabbitmq-bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -11,5 +11,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
 ];
