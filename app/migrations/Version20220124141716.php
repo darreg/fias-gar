e69113c3d4114ba73a2288846960fd7001552ca7
Alex Rundquist <alrund@gmail.com>
2022-01-24 17:30:44 +0300

    Add counter migration

---
+++ app/migrations/Version20220124141716.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220124141716 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE import_counter (type VARCHAR(255) NOT NULL, version_id VARCHAR(255) NOT NULL, task_num INT NOT NULL, parse_error_num INT NOT NULL, parse_success_num INT NOT NULL, save_error_num INT NOT NULL, save_success_num INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(type, version_id))');
+        $this->addSql('COMMENT ON COLUMN import_counter.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN import_counter.updated_at IS \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE import_counter');
+    }
+}
