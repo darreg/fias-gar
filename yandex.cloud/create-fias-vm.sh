ed0e2e9a4ea126d7b1e59495e295861f43915cd4
Alex Rundquist <alrund@gmail.com>
2022-01-26 17:06:03 +0300

    Fix configs

--- yandex.cloud/create-fias-vm.sh
+++ yandex.cloud/create-fias-vm.sh
@@ -2,15 +2,15 @@
 
 export $(grep -v '^#' .env | xargs -d '\n')
 
-yc compute instance create-with-container avitoparser \
+yc compute instance create-with-container fias-gar \
     --zone=ru-central1-b \
     --metadata-from-file user-data=metadata.yaml \
     --network-interface subnet-name=default-ru-central1-b,nat-ip-version=ipv4 \
     --service-account-name $SERVICE_ACCOUNT_NAME \
     --platform standard-v2 \
-    --memory 8GB \
-    --cores 4 \
-    --create-boot-disk type=network-ssd,size=30GB \
-    --docker-compose-file docker-compose-avitoparser.yml \
+    --memory 24GB \
+    --cores 6 \
+    --create-boot-disk type=network-hdd,size=200GB \
+    --docker-compose-file docker-compose-fias-gar.yml \
     --cloud-id $CLOUD_ID \
     --folder-id $FOLDER_ID
