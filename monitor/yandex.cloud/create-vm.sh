d6d3125dd9440b447e7fc429c9479bff54557b37
Alex Rundquist <alrund@gmail.com>
2022-02-07 16:19:43 +0300

    Fix monitor

---
+++ monitor/yandex.cloud/create-vm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export $(grep -v '^#' .env | xargs -d '\n')
+
+yc compute instance create-with-container fias-gar \
+    --zone=ru-central1-b \
+    --metadata-from-file user-data=metadata.yaml \
+    --network-interface subnet-name=default-ru-central1-b,nat-ip-version=ipv4 \
+    --service-account-name $SERVICE_ACCOUNT_NAME \
+    --platform standard-v2 \
+    --memory 4GB \
+    --cores 2 \
+    --create-boot-disk type=network-ssh,size=20GB \
+    --docker-compose-file ../docker-compose.yml \
+    --cloud-id $CLOUD_ID \
+    --folder-id $FOLDER_ID
